e786ecb3aac1a638eca4616c6d225bd6b35d689a
Muhammad-Huzaifa1 <tr1ppyplays123@gmail.com>
2024-03-05 13:53:12 +0500

    work done

---
+++ app.js
@@ -0,0 +1,68 @@
+var num1 = +prompt("Enter your first value");
+var num2 = +prompt("Enter your second value");
+
+
+var add = num1 + num2;
+var minus = num1 - num2;
+var multiply = num1 * num2;
+var divide = num1 / num2;
+var percent = num1 % num2;
+
+
+document.write(
+    "<table border='1'>"+
+    "<tr>"+
+    "<th>"+ "Addition" +"</th>"
+    +"<th>"+ "Subtraction" +"</th>"
+    +"<th>"+ "Multiplication" +"</th>"
+    +"<th>"+ "Division" +"</th>"
+    +"<th>"+ "percentage" +"</th>"
++"</tr>"+
+
+"<tr>"+
+"<td>"+add+"</td>"
++"<td>"+minus+"</td>"
++"<td>"+multiply+"</td>"
++"<td>"+divide+"</td>"
++"<td>"+percent+"</td>"
+
+
+
+
+
+
+
+
+
+
++"</tr>"
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    +"</table>"
+
+
+
+
+
+
+
+
+
+)
